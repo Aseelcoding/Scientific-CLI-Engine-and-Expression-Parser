e33dbdf0d58957c9389bda15ca65af1fa69763aa
Aseel <asilalhashmi@email.com>
2026-05-21 20:01:41 +0700

    #

--- Scientific CLI Engine and Expression Parser.cpp
+++ Scientific CLI Engine and Expression Parser.cpp
@@ -1,6 +1,23 @@
-
 #include <iostream>
+#include <map>
+#include <fstream>
+#include <string>
+#include <stack>
+#include <vector>
 using namespace std;
+
+class calculator
+{
+private :
+	map <string, int> Variables;
+	//
+	map <char, short>precedence = { {'*',4},{'/',4} ,{'+',2} ,{ '-' ,2 } };
+	deque<string> Output;
+	stack<char> Op;
+	//
+
+
+};
 
 
 int main()
